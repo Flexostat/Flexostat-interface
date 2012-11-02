d504319e78ecc7e6fc89f89ed0675edba370f4b5
Chris Takahashi <noemail@example.com>
2012-11-01 23:55:23 -0700

    Release 0.0.4

--- plugins/turbidostatController.py
+++ plugins/turbidostatController.py
@@ -4,22 +4,26 @@ class State:
     def __str__(self):
         return '%.4f' % self.z
 
-def computeControl(self,od,z,time=0.0):
+def computeControl(self,od,z,chamber=0,time=0.0):
     if z == None:
         z = State()
     #calculate control
-    err_sig = 1000*(od-self.cparams['setpoint'])
-    z.z = z.z+err_sig*self.cparams['ki']
+    setpoints = map(float,self.cparams['setpoint'].split())
+    #for debug
+#    print "setpoints: "+ str(setpoints)+ "this: " + str(setpoints[chamber])
+    
+    err_sig = 1000*(od-setpoints[chamber])
+    z.z = z.z+err_sig*float(self.cparams['ki'])
     if z.z<0:
         z.z = 0
-    if z.z>self.cparams['maxdilution']:
-        z.z = self.cparams['maxdilution']
+    if z.z>float(self.cparams['maxdilution']):
+        z.z = float(self.cparams['maxdilution'])
     
-    u = z.z+err_sig*self.cparams['kp']
-    if u < self.cparams['mindilution']:
-        u = self.cparams['mindilution']
-    if u > self.cparams['maxdilution']:
-        u = self.cparams['maxdilution']
+    u = z.z+err_sig*float(self.cparams['kp'])
+    if u < float(self.cparams['mindilution']):
+        u = float(self.cparams['mindilution'])
+    if u > float(self.cparams['maxdilution']):
+        u = float(self.cparams['maxdilution'])
     u = int(u) # make sure u is an int
     
     return (u,z)
