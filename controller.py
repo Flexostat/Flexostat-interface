876092ec7cb112115dae5964ae72bab060bf2888
Chris Takahashi <noemail@example.com>
2012-11-01 23:54:21 -0700

    release 0.0.3

--- controller.py
+++ controller.py
@@ -17,36 +17,43 @@ class Controller:
     def __init__(self,cparams,logfiles,pparams,cport,pport):
         pumpdriver = __import__('plugins.'+pparams['pumpdriver'],globals(),
                                 locals(),['Pump'],-1)
-        _temp = __import__('plugins.'+cparams['controlfun'],globals(),locals,
+        _temp = __import__('plugins.'+cparams['controlfun'],globals(),locals(),
                            ['computeControl'],-1)
         self.computeControl = types.MethodType(_temp.computeControl,self)
-        self.ser_lock = cport.lock
+ #       self.ser_lock = cport.lock
         self.stdout_lock = threading.RLock()
         
         self.pump = pumpdriver.Pump(cparams,logfiles,pparams,cport,pport)
         self.odcal = 1
         
+        #data from config.ini
         self.logfiles = logfiles
         self.pparams = pparams
         self.cparams = cparams #all controller parameters live here
+        #serial ports
         self.serpt = cport
         self.pport = pport
+        
+        #This lock is for the following tx/rx raw values.
+        self.OD_datalock = threading.RLock()
         self.tx_blank = []
         self.rx_blank = []
         self.rx_val = []
         self.tx_val = []
         self.z = []
-        with self.ser_lock:
+        with self.serpt.lock:
             self.serpt.write("clo;")
             self.serpt.flush()
         
         #start the control-loop timer.
+        self.start_time = time();
         self.cont_timer = mytimer(cparams['period'],self.controlLoop)
         self.cont_timer.start()
         
         #start the serial polling timer
         self.ser_timer = mytimer(2, self.serialCheck)
         self.ser_timer.start()
+        #return control to caller
         
     def quit(self):
         self.cont_timer.stop()
@@ -56,7 +63,7 @@ class Controller:
         #if the serpt is uninitialized then do nothing.
         try:
 #no need for lock.  only thread that READS serpt
-#            with self.ser_lock:
+#            with self.serpt.lock:
             while (self.serpt.inWaiting() > 0):
                 line = self.serpt.readline().strip()
                 self.parseline(line)
@@ -93,8 +100,9 @@ class Controller:
                 print s
             #should this be threadsafe?????
             # yes.  it should be.
-            self.tx_val = data[0::2]
-            self.rx_val = data[1::2]
+            with self.OD_datalock:
+                self.tx_val = data[0::2]
+                self.rx_val = data[1::2]
             
     def computeOD(self,btx,brx,tx,rx):
         #calulate OD
@@ -109,7 +117,11 @@ class Controller:
         #if blanks == 0 then use this as blank
         #compute control value
         #do dilution (control valves and pumps)
-        if len(self.rx_val) == 0 or len(self.tx_val) == 0:
+        with self.OD_datalock:
+            tx = self.tx_val
+            rx = self.rx_val
+            
+        if len(rx) == 0 or len(tx) == 0:
             return
         if len(self.tx_blank) == 0 or len(self.rx_blank) == 0:
             try:
@@ -118,8 +130,8 @@ class Controller:
                 self.tx_blank = blank_values[0::2]
                 self.rx_blank = blank_values[1::2]
             except:
-                self.rx_blank = self.rx_val
-                self.tx_blank = self.tx_val
+                self.rx_blank = rx
+                self.tx_blank = tx
                 bf = open('blank.dat','w')
                 #intrleave tx and rx  
                 flat_blank = [j for i in zip(self.tx_blank,self.rx_blank)
@@ -128,28 +140,33 @@ class Controller:
                 for j in flat_blank:
                     bfstring += str(j) + " "
                 bf.write(bfstring + "\n")
+            #setup z when blanking
+            self.z = [None]*len(self.rx_blank)  
                  
         #compute control
         ods = map(self.computeOD,self.tx_blank,self.rx_blank,
-                            self.tx_val,self.rx_val)
-        cont = map(self.computeControl,ods, self.z)
+                            tx,rx)
+        cont = map(self.computeControl,ods, self.z,
+                   [time()-self.start_time]*len(self.z))
         
         u = [q[0] for q in cont]
         self.z = [q[1] for q in cont]
 
-        
-        exf = open('exclude.txt','r')
-        exvals = map(int,exf.readline().split())
-        for exx in exvals:
-            u[exx-1] = 11
-        exf.close()
-        
+        try:
+            exf = open('exclude.txt','r')
+            exvals = map(int,exf.readline().split())
+            exf.close()
+            for exx in exvals:
+                u[exx-1] = 11
+        except:
+            pass
+            
         #log events
-        #TODO: let z be a generic object and have instead log " blah %s"%z
         f = open(self.logfiles['fulllog'],"a")
         s = str(int(round(time())))+" " \
             + str(map(round,ods,[4]*len(ods)))+" " \
-            + str(map(round,self.z,[4]*len(self.z)))+" "+str(u)
+            + '[' + ', '.join([str(Q) for Q in self.z])+"] " \
+            + str(u)
         f.write(s+'\n')
         f.close()
         
@@ -157,7 +174,7 @@ class Controller:
             print s
         
         try:
-            with self.ser_lock:
+            with self.serpt.lock:
                 self.serpt.write("sel0;") #select media source
                 self.serpt.flush()
             print 'sel 0'
@@ -170,25 +187,29 @@ class Controller:
             chamber_num = 1
             
             for dispval in u:
+                if dispval < 1:
+                    chamber_num = chamber_num + 1
+                    continue
+                    
                 selstr = "sel" + str(chamber_num) + ";"
                 #if we're moving from PV1 to PV2 then close first
                 #to prevent leaks into tube 5
                 if chamber_num == 5:
-                    with self.ser_lock:
+                    with self.serpt.lock:
                         self.serpt.write("clo;");
                     sleep(2);
-                with self.ser_lock:
+                with self.serpt.lock:
                     self.serpt.write(selstr) #select chamber
                     self.serpt.flush()
                 sleep(2.0)  #for some reason one PV is very slow.  
                 print selstr #for debug
-                
+                                
                 self.pump.dispense(dispval)
                 self.pump.waitForPumping()
                 
                 chamber_num = chamber_num + 1
             
-            with self.ser_lock:
+            with self.serpt.lock:
                 self.serpt.write("clo;")
                 self.serpt.flush()
             print 'clo'
